3d2bf520c469141017cbd4d2c35dee7a64d30074
rocket <rocket@rocket-pc>
2018-03-09 13:59:21 +0100

    Erster Commit.

---
+++ src/main/java/com/dh/forchheimer_fuchs/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dh.forchheimer_fuchs;
+
+/**
+ *
+ * @author rocket
+ */
+public class Test {
+    //zum Testen, mehr nicht
+}
