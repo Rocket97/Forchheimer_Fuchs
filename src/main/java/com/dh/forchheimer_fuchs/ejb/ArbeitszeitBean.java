b7d49f5b434f0cb197680a329b0d8998f9373974
rocket <rocket@rocket-pc>
2018-03-28 10:46:25 +0200

    git ist...

--- src/main/java/com/dh/forchheimer_fuchs/ejb/ArbeitszeitBean.java
+++ src/main/java/com/dh/forchheimer_fuchs/ejb/ArbeitszeitBean.java
@@ -13,7 +13,13 @@ import java.util.Date;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.ui.ApplicationFrame;
 
 
 /**
@@ -28,33 +34,153 @@ public class ArbeitszeitBean extends EntityBean<Arbeitszeit, Long> {
     }
     
     @RolesAllowed("ff_admin")
-    public JFreeChart stundenAuswertenAlle(List<Benutzer> helfer, StundenKategorie kategorie, Benutzer benutzer){
+    public ApplicationFrame stundenAuswertenAlle(List<Benutzer> helfer, StundenKategorie kategorie){
         
-        // der Admin darf mehr Daten sehen
-        String select = "";
-        if (benutzer.getAdmin()){
-            select = "SELECT COUNT (zeitID) FROM arbeitszeit WHERE";
-            for (Benutzer nutzer : helfer){
-                select = select + " mitgliedsnr = " + nutzer.getMitgliedsnr() + ", ";
-            }
-            select = select + " AND kategorie = " + kategorie;
-        } else {
+        // eine Kategorie, alle Personen
+        
+        // Array für die Zeitspannen, die im Diagramm im Verhältnis dargestellt werden sollen, erzeugen
+        int[] helferminuten = new int[helfer.size()];
+        int endzahlJeKategorie = 0;
+        
+        // Zeitspannen je Arbeitszeit und Kategorie auslesen und addieren
+        for (Benutzer helferlein : helfer){
+            int i = 0;
             
+            // von einem Benutzer alle Zeitspannen der gegebenen Kategorie raussuchen
+            List<Integer> zeitspannen = em.createQuery("SELECT zeitspanne FROM arbeitszeit WHERE a.mitgliedsnr = :mnr AND a.kategorie = :kategorie")
+                .setParameter("mnr", helferlein.getMitgliedsnr())
+                .setParameter("kategorie", kategorie)
+                .getResultList();
+            
+            // Zeitspannen des einen Benutzers addieren
+            for(int zsp : zeitspannen){
+                endzahlJeKategorie = endzahlJeKategorie + zsp;
+            }
+            
+            // die Endsumme abspeichern ( ein Platz je Benutzer)
+            helferminuten[i] = endzahlJeKategorie;
+            
+            // wieder auf Null setzen, um für die nächste Kategorie wieder von vorne die Zeitspannen addieren zu können
+            endzahlJeKategorie = 0;
+            
+            // Zähler hochzählen für den nächsten Benutzer in der Liste
+            i++;
         }
-        em.createQuery(select);
-        return ;
+        
+        // Diagramm erstellen mit Hilfe von ChartBean
+        JFreeChart pieChart = tortendiagrammErstellen(kategorie.toString(), helferminuten);
+        JFreeChart barChart = tortendiagrammErstellen(kategorie.toString(), helferminuten);
+        
+        return setzeFrameFuerDiagramme(pieChart, barChart);
     }
     
-    public JFreeChart stundenAuswertenEinzeln(Benutzer helfer, Benutzer benutzer){
+    public ApplicationFrame stundenAuswertenEinzeln(Benutzer benutzer){
         
-        // der Admin darf mehr Daten sehen
-        if (benutzer.getAdmin()){
-            int zahl = em.createQuery("SELECT zeitspanne FROM arbeitszeit WHERE mitgliedsnr = :mnr AND kategorie = :kategorie");
+        // eine Person, alle Kategorien
+        
+//      Kategorien in dem Array sind wie folgt zugeordnet:
+//      ausWeiterbildung [0], hausinstandhaltung [1], verwaltungsarbeit [2], materialpflege [3], fahrzeugwartung [4], jrkVerwaltungsarbeit [5], notfalldarstellung [6],
+//      schulsanitätsdienst [7], bereitschaftsabend [8], jahreshauptversammlung [9], verwaltungssitzung [10], kameradschaftspflege[11], jugendbereitschaftsabend [12]
+
+        // Array für die Zeitspannen, die im Diagramm im Verhältnis dargestellt werden sollen, erzeugen
+        int[] kategorie = new int[13];
+        int endzahlJeKategorie = 0;
+        
+        // Zeitspannen je Arbeitszeit und Kategorie auslesen und addieren
+        for (int i=0; i<StundenKategorie.values().length; i++){
+            List<Integer> zeitspannen = em.createQuery("SELECT zeitspanne FROM arbeitszeit WHERE a.mitgliedsnr = :mnr AND a.kategorie = :kategorie")
+                .setParameter("mnr", benutzer.getMitgliedsnr())
+                .setParameter("kategorie", StundenKategorie.values()[i])
+                .getResultList();
+            
+            // Zeitspannen einer Kategorie addieren
+            for(int zsp : zeitspannen){
+                endzahlJeKategorie = endzahlJeKategorie + zsp;
+            }
+            kategorie[i] = endzahlJeKategorie;
+            
+            // wieder auf Null setzen, um für die nächste Kategorie wieder von vorne die Zeitspannen addieren zu können
+            endzahlJeKategorie = 0;
         }
-        return ;
+        
+        // Diagramm erstellen mit Hilfe von ChartBean
+        JFreeChart pieChart = tortendiagrammErstellen(benutzer.getBenutzername(), kategorie);
+        JFreeChart barChart = tortendiagrammErstellen(benutzer.getBenutzername(), kategorie);
+        
+        return setzeFrameFuerDiagramme(pieChart, barChart);
+    }
+    
+    /*==================
+    *  TORTENDIAGRAMM
+    * ==================*/
+    
+    public static JFreeChart tortendiagrammErstellen (String titel, int[] kategorie){
+        
+        // Datenset erzeugen
+        DefaultPieDataset pieDataset = new DefaultPieDataset();
+        
+        // Daten in das Dataset laden 
+        for (int i = 0; i < kategorie.length; i++){
+            pieDataset.setValue(StundenKategorie.values()[i], kategorie[i]);
+        }
+        
+        // ein Tortendiagramm mit den Daten aus dem Datenset erzeugen
+        JFreeChart chart = ChartFactory.createPieChart
+            (titel, // Title
+            pieDataset, // Dataset
+            true,// legend
+            false,// tooltips
+            false// URL
+        );
+        
+        return chart;
+        
+    }
+    
+    
+    /*==================
+    *   BALKENDIAGRAMM
+    * ==================*/
+    
+    public JFreeChart balkendiagrammErstellen(String titel, int[] kategorie){
+        
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        // dataset.addValue();
+        
+        for (int i = 0; i < kategorie.length; i++){
+            dataset.addValue(kategorie[i], "Arbeitszeit", StundenKategorie.values()[i]);
+        }
+        
+        JFreeChart chart = ChartFactory.createBarChart
+            (titel, // Titel
+            "Kategorien",// x-Achsen-Beschriftung
+            "Anzahl der Minuten", //y-Achsen-Beschriftung
+            dataset, // Dataset
+            PlotOrientation.VERTICAL, // Balken-Ausrichtung
+            true, // Legende anzeigen?
+            true, // Tooltips?
+            false // URLs
+        );
+        return chart;
+    }   
+    
+//    =======================================
+//    Nur, wenn die JSP den Chart nicht nimmt
+//    =======================================
+    
+    public ApplicationFrame setzeFrameFuerDiagramme(JFreeChart pieChart, JFreeChart barChart){
+        ApplicationFrame frame = new ApplicationFrame("Diagramme");
+
+        ChartPanel pieChartPanel = new ChartPanel(pieChart);
+        ChartPanel barChartPanel = new ChartPanel(barChart);
+        frame.setContentPane(pieChartPanel);
+        frame.setContentPane(barChartPanel);
+        frame.pack();
+        frame.setVisible(true);
+        
+        return frame;
     }
         
-    
     public int berechneZeitspanne(Calendar from, Calendar to) {
         Date fromDate = from.getTime();
         Date toDate = to.getTime();
